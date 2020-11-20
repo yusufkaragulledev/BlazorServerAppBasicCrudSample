98237a3cefa4efbdd9abb0c91b8c5854f921bdce
Yusuf Karagulle <yusuf.karagulle@hipposoft.net>
2020-11-20 22:21:42 +0300

    code editing

--- Services/ICustomerService.cs
+++ Services/ICustomerService.cs
@@ -1,9 +1,7 @@
+using BlazorServerAppBasicCrudSample.Entities;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using BlazorServerAppBasicCrudSample.Data;
-using BlazorServerAppBasicCrudSample.Entities;
-using Microsoft.EntityFrameworkCore;
 
 namespace BlazorServerAppBasicCrudSample.Services
 {
@@ -14,37 +12,5 @@ namespace BlazorServerAppBasicCrudSample.Services
         Task CreateCustomer(Customer customer);
         Task UpdateCustomer(Customer customer);
         Task DeleteCustomer(Customer customer);
-    }
-
-    public class CustomerService : ICustomerService
-    {
-        private readonly MyDbContext _dbContext;
-
-        public CustomerService(MyDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
-        
-        public async Task<IEnumerable<Customer>> GetAllCustomer() => await _dbContext.Customer.ToListAsync();
-
-        public async Task<Customer> GetByIdCustomer(Guid id) => await _dbContext.Customer.FirstOrDefaultAsync(x => x.Id == id);
-
-        public async Task CreateCustomer(Customer customer)
-        {
-            await _dbContext.Customer.AddAsync(customer);
-            await _dbContext.SaveChangesAsync();
-        }
-
-        public async Task UpdateCustomer(Customer customer)
-        {
-            _dbContext.Customer.Update(customer);
-            await _dbContext.SaveChangesAsync();
-        }
-
-        public async Task DeleteCustomer(Customer customer)
-        {
-            _dbContext.Customer.Remove(customer);
-            await _dbContext.SaveChangesAsync();
-        }
     }
 }
